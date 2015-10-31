40a07d9ecf8c50742609a0d9f7aebc481c53ef95
Luis en Linux <luissanhez0305@gmail.com>
2015-10-31 05:27:50 -0700

    info de trips por ruta en las paradas de destino y origne

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
 <?php
-$url="http://54.174.147.117:3000/v1/trips";
+if(isset($_GET['from']) && isset($_GET['to']))
+{
+$from = $_GET['from'];
+$to = $_GET['to'];
+$urlFrom="http://54.174.147.117:3000/v1/stops/$from?with_stop_sequence=true";
+$urlTo="http://54.174.147.117:3000/v1/stops/$to?with_stop_sequence=true";
+
 //  Initiate curl
-$ch = curl_init();
+$chFrom = curl_init();
 // Disable SSL verification
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($chFrom, CURLOPT_SSL_VERIFYPEER, false);
 // Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($chFrom, CURLOPT_RETURNTRANSFER, true);
 // Set the url
-curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($chFrom, CURLOPT_URL,$urlFrom);
+
+//  Initiate curl
+$chTo = curl_init();
+// Disable SSL verification
+curl_setopt($chTo, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+curl_setopt($chTo, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($chTo, CURLOPT_URL,$urlTo);
+
 // Execute
-$result=curl_exec($ch);
+$resultFrom=curl_exec($chFrom);
+$resultTo=curl_exec($chTo);
 // Closing
-curl_close($ch);
+curl_close($chFrom);
+curl_close($chTo);
 
 // Will dump a beauty json :3
-var_dump(json_decode($result, true));
+$stopFrom = json_decode($resultFrom, true);
+$stopTo = json_decode($resultTo, true);
+echo '<p>***************FROM:</p><p>'.print_r($stopFrom['data']['routes']).'</p>';
+echo '***************TO:</br>'.print_r($stopTo['data']['routes']);
+}
 ?>
